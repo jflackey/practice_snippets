32b9c664588615c8129ba6ddda48484c2750fd1d
jflackey <44788475+jflackey@users.noreply.github.com>
2020-02-23 17:57:40 -0800

    Add file via upload

---
+++ different_ways_to_test_multiple_flags.py
@@ -0,0 +1,14 @@
+x, y, z = 0, 1, 0
+
+if x == 1 or y == 1 or z == 1:
+    print('passed')
+
+if 1 in (x, y, z):
+    print('passed')
+
+# Test for truthiness:
+if x or y or z:
+    print('passed')
+
+if any((x, y, z)):
+    print('passed')
