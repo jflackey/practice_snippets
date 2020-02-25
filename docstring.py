fc27920b9a96f56484c2643ce70fff72e447cc2d
jflackey <44788475+jflackey@users.noreply.github.com>
2020-02-24 17:16:51 -0800

    Add file via upload

---
+++ docstring.py
@@ -0,0 +1,10 @@
+def practice_func(s):
+    """Display an argument value."""
+    print(s)
+
+# Display function docstring
+print('Display function docstring')
+
+# Display an argument value.
+print(practice_func.__doc__)
+
