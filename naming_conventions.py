12d5dff912339ec3f549491fe99f863767303695
jflackey <44788475+jflackey@users.noreply.github.com>
2020-02-24 17:53:05 -0800

    Add file to upload

---
+++ naming_conventions.py
@@ -0,0 +1,17 @@
+# Naming conventions
+
+# Text string
+xxx_str = 'text string'
+s, s1, s2 = ('text string', 'text string1', 'text string2')
+
+# List
+xxx_list = ['list naming convention']
+
+# Floating-point numbers
+x,y,z = 1.0, 2.0, 3.0
+
+# Integers
+i, j, k, m = 1, 2, 3, 4
+
+# Function active verb
+# convert_to_centigrade()
